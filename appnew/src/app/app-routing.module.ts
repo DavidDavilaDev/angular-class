4d1050a4ad7cebdb129e2b89d60d16bc4b5d91db
Guayaba15 <daviddavilabarrios@gmail.com>
2024-05-24 15:41:08 -0600

    act 2

--- appnew/src/app/app-routing.module.ts
+++ appnew/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TableComponent } from './view/hero/table/table.component';
+import { FormComponent } from './view/hero/form/form.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:"", redirectTo:"/dashboard", pathMatch:"full"},
+  {path:"dashboard", component:DashboardComponent},
+  {path:"tabla", component:TableComponent},
+  {path:"form", component:FormComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
